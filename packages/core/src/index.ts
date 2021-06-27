6eb330408e909c9880a7fecfcad698ff9b8d7967
Juan Jose Carracedo <Juan.Carracedo@fathom3.com>
2021-06-27 10:03:50 +0200

    chore: add core config

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,23 @@
-export function awesomeFn() {
-  return "Hello";
-}
+export { 
+  DefaultCompareKeysFunction, 
+  DefaultCheckKeyEquality, 
+  DefaultCheckValueEquality, 
+  DefaultCompareValues, 
+  CreateRandomSortedIndex , 
+  GetRowsArrayFromHeight, 
+  CreateRefArrayFromTreeHeight,
+  ArrayDuplicate,
+  IsEmpty,
+  GetObjectValue,
+  CompareArray,
+  ExpandObject,
+  ArrObjectsDuplicates,
+  FlattenArray,
+  CompressObject,
+  GetUUID,
+  GetDate,
+  GetObjValue,
+  SaveArrayDups,
+  MergeSort,
+  GetSortType
+} from './utils'
