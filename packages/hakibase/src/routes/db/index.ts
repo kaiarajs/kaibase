a7b863ad1a59ebe4404d2b43539755678abff664
DonsWayo <djwayomix@gmail.com>
2021-07-26 23:24:45 +0200

    chore: adjust db responses

--- packages/hakibase/src/routes/db/index.ts
+++ packages/hakibase/src/routes/db/index.ts
@@ -14,18 +14,21 @@ const hakibase: FastifyPluginAsync =async (fastify, opts): Promise<void> => {
   fastify.get<{Headers: Headers, Querystring: Querystring}>('/', async function (request, reply) {
     const { filter } = request.query
     const collection = request.headers['collection']
-    return fastify.mongo.db(CONFIG.databaseName).collection(collection).find(filter || {}).toArray();
+    const db = await fastify.mongo.db(CONFIG.databaseName).collection(collection).find(filter || {}).toArray();
+    return reply.status(200).send(db)
   }),
   fastify.post<{Headers: Headers}>('/', async function (request, reply) {
     const body = request.body as Document
     const collection = request.headers['collection']
     if(!body) return reply.status(403).send({ status: 403, msg: "body is empty!"})
-    return fastify.mongo.db(CONFIG.databaseName).collection(collection).insertOne(body)
+    const db = await fastify.mongo.db(CONFIG.databaseName).collection(collection).insertOne(body)
+    return reply.status(201).send(db)
   }),
   fastify.delete<{Headers: Headers, Querystring: Querystring}>('/', async function (request, reply) {
     const { filter } = request.query
     const collection = request.headers['collection']
-    return fastify.mongo.db(CONFIG.databaseName).collection(collection).deleteOne(filter || {});
+    const db = await fastify.mongo.db(CONFIG.databaseName).collection(collection).deleteOne(filter || {});
+    return reply.status(201).send(db)
   })
 }
 
