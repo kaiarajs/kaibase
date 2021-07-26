819d538ba5872c9f4ff9c0b117f93712bee8d03d
DonsWayo <djwayomix@gmail.com>
2021-07-26 15:57:05 +0200

    adjust mongo plugin on close

--- packages/hakibase/src/plugins/mongo.ts
+++ packages/hakibase/src/plugins/mongo.ts
@@ -15,6 +15,7 @@ export default fp<SupportPluginOptions>(async (fastify, opts) => {
     const client = new MongoClient(uri);
     await client.connect();
     fastify.decorate('mongo', client);
+    fastify.addHook('onClose', () => client.close());
   } catch (error) {
     console.log(error);
     process.exit(1);
