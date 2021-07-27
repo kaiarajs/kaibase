a2099b004a4d5650b25f6d9753e0d189c9c64c22
DonsWayo <djwayomix@gmail.com>
2021-07-27 13:32:09 +0200

    chore: adjust jwt plugin

--- packages/hakibase/src/plugins/jwt.ts
+++ packages/hakibase/src/plugins/jwt.ts
@@ -1,9 +1,23 @@
 import fp from 'fastify-plugin'
 import fastifyJWT, { FastifyJWTOptions } from 'fastify-jwt'
+import { FastifyReply, FastifyRequest } from 'fastify'
 
 
 export default fp<FastifyJWTOptions>(async (fastify, opts) => {
   fastify.register(fastifyJWT, {
     secret: "supersecret"
   })
+  fastify.decorate("authenticate", async function(request: FastifyRequest, reply: FastifyReply) {
+    try {
+      await request.jwtVerify()
+    } catch (err) {
+      reply.send(err)
+    }
+  })
 })
+
+declare module 'fastify' {
+  export interface FastifyInstance {
+    authenticate: any
+  }
+}
