e08d0adb3cb8320825ddea91497da08276109a23
DonsWayo <djwayomix@gmail.com>
2021-07-29 11:55:44 +0200

    chore: add storage

--- packages/hakibase/src/routes/storage/index.ts
+++ packages/hakibase/src/routes/storage/index.ts
@@ -1,8 +1,73 @@
-import { FastifyPluginAsync } from "fastify"
+import { FastifyInstance, FastifyPluginAsync } from "fastify"
+import { CONFIG } from "../../app"
 
-const storage: FastifyPluginAsync =async (fastify, opts): Promise<void> => {
-  fastify.get('/', async function (request, reply) {
-    return 'this is an example'
+interface HeadersStorage {
+  location: string;
+}
+
+
+const StorageOpts = (fastify: FastifyInstance) => {
+  return {
+    schema: {
+      consumes: ['multipart/form-data'],
+      headers: {
+        properties: {
+          location: {
+            type: 'string',
+            maxLength: 80,
+            minLength: 3
+          },
+        },
+        required: ['location']
+      },
+      body: {
+        properties: {
+          file: { isFileType: true },
+        },
+        required: ['file']
+      }
+    },
+    preValidation: [fastify.authenticate, fastify.secureRoles]
+  }
+}
+
+const storage: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+  fastify.post<{ Headers: HeadersStorage }>('/', StorageOpts(fastify), async function (request, reply) {
+    const location = request.headers['location'];
+    const data = await request.file();
+    const buffer = await data.toBuffer();
+    const upload = await fastify.storage.disk(CONFIG.storage.driver).put(location, buffer);
+    reply.send(upload);
+  }),
+  fastify.get<{ Headers: HeadersStorage }>('/', StorageOpts(fastify), async function (request, reply) {
+    const location = request.headers['location'];
+    const upload = await fastify.storage.disk(CONFIG.storage.driver).get(location);
+    reply.send(upload);
+  }),
+  fastify.get<{ Headers: HeadersStorage }>('/signedUrl', StorageOpts(fastify), async function (request, reply) {
+    const location = request.headers['location'];
+    const upload = await fastify.storage.disk(CONFIG.storage.driver).getSignedUrl(location);
+    reply.send(upload);
+  }),
+  fastify.get<{ Headers: HeadersStorage }>('/exists', StorageOpts(fastify), async function (request, reply) {
+    const location = request.headers['location'];
+    const upload = await fastify.storage.disk(CONFIG.storage.driver).exists(location);
+    reply.send(upload);
+  }),
+  fastify.get<{ Headers: HeadersStorage }>('/getBuffer', StorageOpts(fastify), async function (request, reply) {
+    const location = request.headers['location'];
+    const upload = await fastify.storage.disk(CONFIG.storage.driver).getBuffer(location);
+    reply.send(upload);
+  }),
+  fastify.get<{ Headers: HeadersStorage }>('/getStat', StorageOpts(fastify), async function (request, reply) {
+    const location = request.headers['location'];
+    const upload = await fastify.storage.disk(CONFIG.storage.driver).getStat(location);
+    reply.send(upload);
+  }),
+  fastify.delete<{ Headers: HeadersStorage }>('/signedUrl', StorageOpts(fastify), async function (request, reply) {
+    const location = request.headers['location'];
+    const upload = await fastify.storage.disk(CONFIG.storage.driver).delete(location);
+    reply.status(201).send(upload);
   })
 }
 
