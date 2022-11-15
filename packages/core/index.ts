83c5fd17b87982a45da664f68a58328a8b8bd3c5
DonsWayo <34981526+DonsWayo@users.noreply.github.com>
2022-11-15 16:04:53 +0100

    chore: added core to turborepo

---
+++ packages/core/index.ts
@@ -0,0 +1,23 @@
+export { 
+    DefaultCompareKeysFunction, 
+    DefaultCheckKeyEquality, 
+    DefaultCheckValueEquality, 
+    DefaultCompareValues, 
+    CreateRandomSortedIndex , 
+    GetRowsArrayFromHeight, 
+    CreateRefArrayFromTreeHeight,
+    ArrayDuplicate,
+    IsEmpty,
+    GetObjectValue,
+    CompareArray,
+    ExpandObject,
+    ArrObjectsDuplicates,
+    FlattenArray,
+    CompressObject,
+    GetUUID,
+    GetDate,
+    GetObjValue,
+    SaveArrayDups,
+    MergeSort,
+    GetSortType
+  } from './utils'
