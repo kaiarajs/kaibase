e7768faf78035322e96e0839edc8d2e09c320d76
DonsWayo <34981526+DonsWayo@users.noreply.github.com>
2022-11-21 10:49:57 +0100

    chore: add more methods to model

--- packages/kaigoose/src/model.ts
+++ packages/kaigoose/src/model.ts
@@ -1,11 +1,13 @@
+import { UpdateOptions } from "@kaiarajs/kaibase/dist/types";
 import { Schema } from "@kaiarajs/schema";
 import { Kaigoose } from "./kaigoose";
 
 
 
 export class Model  {
-    name;
-    schema;
+    name: string;
+    schema: Object;
+
     constructor(name: string, schema: Object) {
         this.name = name;
         this.schema = schema;
@@ -19,4 +21,21 @@ export class Model  {
        this.validate(value);
        return await Kaigoose.kaibase.collection(this.name).insert(value)
     }
+
+    async find(query?: any, sort?: Record<string, unknown>, skip?: number, limit?: number) {
+        return await Kaigoose.kaibase.collection(this.name).find(query).sort(sort).skip(skip || 0).limit(limit || 100).exec();
+    }
+
+    async findById(id: string) {
+        return await Kaigoose.kaibase.collection(this.name).find({id}).exec();
+    }
+
+    async deleteById(id: string) {
+        return await Kaigoose.kaibase.collection(this.name).remove({id});
+    }
+
+
+    async update(query: Record<string, unknown>, updateContent: any, updateOptions?: UpdateOptions) {
+        return await Kaigoose.kaibase.collection(this.name).update(query, updateContent, updateOptions)
+    } 
 }
